fc6aaab8b05fcb479804c6cd090f83731080a4f4
shayaan-khan-astero <shayaan.astero@gmail.com>
2025-06-19 13:38:48 +0530

    add null checks for image modal elements to prevent errors

--- src/js/layout/image-modal.js
+++ src/js/layout/image-modal.js
@@ -2,6 +2,10 @@ export const imageModal = () => {
   const imageModal = document.getElementById('imageModal')
   const modalImage = document.getElementById('modalImage')
 
+  if (!imageModal || !modalImage) {
+    return
+  }
+
   imageModal.addEventListener('show.bs.modal', function (event) {
     const trigger = event.relatedTarget
     const imgSrc = trigger.getAttribute('data-img-src')
